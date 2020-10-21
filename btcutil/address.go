d3d9e97d639b860c23d7d6b2763497de8fed6168
paolocappelletti <paolo@horizenlabs.io>
2020-10-21 15:28:05 +0200

    removed btcutil packages

--- btcutil/address.go
+++ btcutil/address.go
@@ -7,11 +7,10 @@ package btcutil
 import (
 	"encoding/hex"
 	"errors"
-	"fmt"
+
 	"github.com/HorizenOfficial/rosetta-zen/btcd/btcec"
 	"github.com/HorizenOfficial/rosetta-zen/btcd/chaincfg"
 	"github.com/HorizenOfficial/rosetta-zen/btcutil/base58"
-	"github.com/HorizenOfficial/rosetta-zen/btcutil/bech32"
 	"golang.org/x/crypto/ripemd160"
 )
 
@@ -124,49 +123,6 @@ func DecodeAddress(addr string, defaultNet *chaincfg.Params) (Address, error) {
 	default:
 		return nil, errors.New("decoded address is of unknown size")
 	}
-}
-
-// decodeSegWitAddress parses a bech32 encoded segwit address string and
-// returns the witness version and witness program byte representation.
-func decodeSegWitAddress(address string) (byte, []byte, error) {
-	// Decode the bech32 encoded address.
-	_, data, err := bech32.Decode(address)
-	if err != nil {
-		return 0, nil, err
-	}
-
-	// The first byte of the decoded address is the witness version, it must
-	// exist.
-	if len(data) < 1 {
-		return 0, nil, fmt.Errorf("no witness version")
-	}
-
-	// ...and be <= 16.
-	version := data[0]
-	if version > 16 {
-		return 0, nil, fmt.Errorf("invalid witness version: %v", version)
-	}
-
-	// The remaining characters of the address returned are grouped into
-	// words of 5 bits. In order to restore the original witness program
-	// bytes, we'll need to regroup into 8 bit words.
-	regrouped, err := bech32.ConvertBits(data[1:], 5, 8, false)
-	if err != nil {
-		return 0, nil, err
-	}
-
-	// The regrouped data must be between 2 and 40 bytes.
-	if len(regrouped) < 2 || len(regrouped) > 40 {
-		return 0, nil, fmt.Errorf("invalid data length")
-	}
-
-	// For witness version 0, address MUST be exactly 20 or 32 bytes.
-	if version == 0 && len(regrouped) != 20 && len(regrouped) != 32 {
-		return 0, nil, fmt.Errorf("invalid data length for witness "+
-			"version 0: %v", len(regrouped))
-	}
-
-	return version, regrouped, nil
 }
 
 // AddressPubKeyHash is an Address for a pay-to-pubkey-hash (P2PKH)
