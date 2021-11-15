c4608fbebadff500a23a6b3c224d9bc1793504db
Yuriko <yurikoainaba@gmail.com>
2021-11-15 11:44:27 -0500

    Remove unneeded file

--- launch_docker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-sudo docker rm -f testing
-sudo make build-local
-sudo docker run -d --name=testing --ulimit "nofile=100000:100000" -v "$(pwd)/zen-data:/data" -e "MODE=ONLINE" -e "NETWORK=REGTEST" -e "PORT=8080" -p 8080:8080 -p 18333:18333 rosetta-zen:latest sleep 9999
-sudo docker exec -it testing bash
-
