bde417a160518b8e430d285878af86a0e8d97c8c
cronicc <cronic@zensystem.io>
2023-11-21 14:59:32 +0000

    Fetch GPG keys one by one

--- ci/setup_env.sh
+++ ci/setup_env.sh
@@ -3,13 +3,17 @@
 set -eo pipefail
 
 if [ ! -z "${TRAVIS_TAG}" ]; then
-  export GNUPGHOME="$(mktemp -d 2>/dev/null || mktemp -d -t 'GNUPGHOME')"
+  GNUPGHOME="$(mktemp -d 2>/dev/null || mktemp -d -t 'GNUPGHOME')"
+  export GNUPGHOME
   echo "Tagged build, fetching maintainer keys."
-  gpg -v --batch --keyserver hkps://keys.openpgp.org --recv $ROSETTA_MAINTAINER_KEYS ||
-  gpg -v --batch --keyserver keyserver.ubuntu.com --recv $ROSETTA_MAINTAINER_KEYS ||
-  gpg -v --batch --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys $ROSETTA_MAINTAINER_KEYS ||
-  gpg -v --batch --keyserver hkp://ipv4.pool.sks-keyservers.net --recv-keys $ROSETTA_MAINTAINER_KEYS ||
-  gpg -v --batch --keyserver hkp://pgp.mit.edu:80 --recv-keys $ROSETTA_MAINTAINER_KEYS
+  read -a keys <<< "$ROSETTA_MAINTAINER_KEYS"
+  for key in "${keys[@]}"; do
+    gpg -v --batch --keyserver keyserver.ubuntu.com --recv "$key" ||
+    gpg -v --batch --keyserver hkps://keys.openpgp.org --recv "$key" ||
+    gpg -v --batch --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys "$key" ||
+    gpg -v --batch --keyserver hkp://ipv4.pool.sks-keyservers.net --recv-keys "$key" ||
+    gpg -v --batch --keyserver hkp://pgp.mit.edu:80 --recv-keys "$key"
+  done
   if git verify-tag -v "${TRAVIS_TAG}"; then
     echo "Valid signed tag"
     export version="${TRAVIS_TAG}"
